57291bd5a697edd361d1d56a2c16be8ff93900d8
Somu-ed <satpathysoumyajeet.99@gmail.com>
2021-01-08 15:29:06 +0530

    DOM manipulation with Js

---
+++ home.js
@@ -0,0 +1,17 @@
+console.log('Hello');
+
+// alert('yohh this is somu');
+
+// write comment inline
+
+// Variables - these are how we same and move around data
+var s = 'pizza';
+console.log(s);
+
+var num = 45;
+console.log(num);
+
+// DOM manupulation
+var age = prompt('What is your age?');
+
+document.getElementById('stext').innerHTML = age;
