865cf4217760bd946ed8480763c98187b3b88989
Somu-ed <satpathysoumyajeet.99@gmail.com>
2021-01-08 15:39:06 +0530

    Numbers in javascript

--- home.js
+++ home.js
@@ -9,9 +9,30 @@ var s = 'pizza';
 console.log(s);
 
 var num = 45;
-console.log(num);
+// console.log(num);
 
 // DOM manupulation
-var age = prompt('What is your age?');
+/* Just a technical way of saying
+change HTML with Javascript */
 
-document.getElementById('stext').innerHTML = age;
+// var age = prompt('What is your age?');
+
+// document.getElementById('stext').innerHTML = age;
+
+// Numbers in Javascript
+var num1 = 10;
+
+// Increment by 11
+num1+=11;
+
+//Decrement by 2
+num1-=2;
+console.log(num1);
+
+// Divide /, multiply *, remainder %
+console.log(num1 % 6);
+
+// increment or decrement by 1
+num1++;  // Inc
+num1--;  // Dec
+
